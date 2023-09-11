7f60911c972b6edc8528ab3711ffffc0793c0d89
kiminsik <insikkim1234@gmail.com>
2023-09-11 12:45:48 +0900

    0911 오전

---
+++ javawork/NaverJavaProject/src/bit701/day0911/Ex7_Constructor.java
@@ -0,0 +1,29 @@
+package bit701.day0911;
+
+public class Ex7_Constructor {
+
+	public static void main(String[] args) {
+		/*방법1메모리 할당 후 생성
+		Student []stu=new Student[3];//배열을 5개로 할당했지만 저장값은 bnull
+		stu[0]=new Student();
+		stu[1]=new Student("이효리");
+		stu[2]=new Student("이진",1989,99);*/
+		
+		//방법2
+		
+		Student []stu= {
+				new Student(),
+				new Student("유재석"),
+				new Student("이효리",1989,89),
+				new Student("캔디",1979,100)
+		};
+		
+		/*for(int i=0;i<stu.length;i++)
+			stu[i].studentInfo();*/
+		
+		for(Student s:stu)
+			s.studentInfo();
+
+	}
+
+}
---
+++ javawork/NaverJavaProject/src/bit701/day0911/MyStudent.java
@@ -0,0 +1,68 @@
+package bit701.day0911;
+
+public class MyStudent {
+	private String name;
+	private int kor,eng;
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+	public void setKor(int kor)
+	{
+		this.kor=kor;
+	}
+	public void setEng(int eng)
+	{
+		this.eng=eng;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	public int getKor()
+	{
+		return kor;
+	}
+	public int getEng()
+	{
+		return eng;
+	}
+	
+	
+	public MyStudent(String name,int kor,int eng) {
+		this.name=name;
+		this.kor=kor;
+		this.eng=eng;
+	}
+	
+	//두과목의 합계를 구해서 리턴
+	public int getTotal()
+	{
+		return kor+eng;
+	}
+	
+	//두 과목의 평균
+	public double getAverage()
+	{
+		return getTotal()/2.0;
+	}
+	
+	//평균에 따른 등급 리턴
+	public String getGrade()
+	{
+		double avg=this.getAverage();
+		if(avg>=90)
+		{
+			return "a그룹";
+		}
+		else if(avg>=80)
+		{
+			return "b그룹";
+		}
+		else {
+			return "탈락";
+		}
+	}
+
+}
